e48aed8e7e8226a2371b47404839b39ca0cb5b0f
kingsley-people-box <kingsley@peoplebox.ai>
2024-04-15 12:56:10 +0100

    add buider for boxes

---
+++ lib/qippet/box/code_box.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "./box"
+
+module Qippet
+  class LineBox < Box
+  end
+
+  class CodeBox < Box
+  end
+end
--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require "qippet/builder"
+require "qippet/extract"
+require "qippet/box/container_box"
+# autoload :ContainerBox, "qippet/box/container_box"
 
 RSpec.describe Qippet::Builder do
   it "should build qippet image" do
@@ -8,7 +11,14 @@ RSpec.describe Qippet::Builder do
     expect(data).to eq("")
   end
 
-  it "should be able to call specific builder" do
-    expect { Qippet::Builder.build_from_xml_node }.to raise_error(NoMethodError)
+  it "should build boxes" do
+    file_path = "spec/assets/pop.xml"
+    extracted_content = Qippet::Extraction::Extract.from_file(file_path)
+
+    node = Qippet::Builder.setup_node extracted_content[:root]
+
+    expect(node).to be_a Qippet::ContainerBox
+    expect(node.children.length).to equal 2
+    expect(node.children[0].children.length).to equal 1
   end
 end
