fc87c3c79baf5d347a280e1bbe8dda5249b4b06c
kingsley-people-box <kingsley@peoplebox.ai>
2024-04-21 17:01:28 +0100

    implement code writing

--- lib/qippet/boxes/code_box.rb
+++ lib/qippet/boxes/code_box.rb
@@ -5,23 +5,76 @@ require "qippet/extract"
 
 module Qippet
   module Boxes
-    class LineBox < Box
-    end
-
     # Box for creating code
     class CodeBox < Box
-      def fetch_code
-        fetch_code_from_file
+      attr_reader :code_lines, :show_line, :writer
+
+      CODE_LINE_HEIGHT = 25
+      CODE_TAB = 20
+
+      def initialize
+        super
+        @show_line = true
+      end
+
+      def add_child(*)
+        throw :no_children_for_node, "Code Box cannot contain chidren"
       end
 
       def fetch_code_from_file
-        code_file_path = "/code/check.rb"
-        Extract.from_file(code_file_path)
+        code_file_path = "./Rakefile"
+        code_content = Extract.from_file(code_file_path)
+        return nil if code_content.nil?
+
+        @code_lines = code_content.split("\n")
       end
 
       def render
         fetch_code_from_file
-        super
+        return nil if code_lines.nil?
+
+        setup_writer
+        write_code_on_image super
+      end
+
+      private
+
+      def write_code_on_image(image)
+        code_lines.each_with_index do |line, index|
+          @line_tab = PADDING_X
+          add_line_number(index + 1) if show_line
+
+          add_line_indentation line
+          writer.annotate(image, 0, 0, @line_tab, CODE_LINE_HEIGHT * (index + 1), line)
+        end
+        image
+      end
+
+      def add_line_number(number)
+        writer.annotate(image, 0, 0, @line_tab, CODE_LINE_HEIGHT * number, number.to_s)
+        @line_tab += 40
+      end
+
+      def add_line_indentation(line)
+        line.chars.each do |c|
+          break if c != " "
+
+          @line_tab += 10
+        end
+      end
+
+      def setup_writer
+        @writer = Magick::Draw.new
+        @writer.font_family = "helvetica"
+        @writer.pointsize = 15
+      end
+
+      def image_height(*)
+        (code_lines.length * CODE_LINE_HEIGHT) + PADDING
+      end
+
+      def image_width(*)
+        ((code_lines.max { |a, b| a.length <=> b.length }&.length || 0) * 10) + PADDING
       end
     end
   end
