d4903bdc1c5ba528479451ad1da6e53cd6656726
kingsley-people-box <kingsley@peoplebox.ai>
2024-04-29 14:44:12 +0100

    implement fetch from github file

--- lib/qippet/extract.rb
+++ lib/qippet/extract.rb
@@ -1,12 +1,28 @@
 # frozen_string_literal: true
 
+require "uri"
+require "net/http"
+require "base64"
+require "json"
+
 module Qippet
   # Extract Class
   class Extract
     class << self
+      GITHUB_BASE_API_URL = "https://api.github.com/repos/".freeze
+
       def from_file(path)
         path = Pathname.new(Dir.pwd).join(path)
         read_file(path)
+      end
+
+      def from_github(path = "rails/rails/contents/version.rb")
+        url = URI("#{GITHUB_BASE_API_URL}#{path}")
+        response = Net::HTTP.get_response(url)
+        response = JSON.parse(response.body)
+        Base64.decode64(response["content"])
+      rescue StandardError
+        nil
       end
 
       private
--- spec/extract_spec.rb
+++ spec/extract_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Qippet::Extract do
     file_path = "spec/assets/pop.xml"
     expect(Qippet::Extract.from_file(file_path)).not_to be nil
   end
+
+  it "should return content from valid github path" do
+    mock_response = Object.new
+    def mock_response.body
+      "{\"content\": \"IyBmcm96ZW5fc3RyaW5nX2xpdGVyYWw6IHRydWUKCm1vZHVsZSBSYWlscwog\\nICMgUmV0dXJucyB0aGUgY3VycmVudGx5IGxvYWRlZCB2ZXJzaW9uIG9mIFxS\\nYWlscyBhcyBhICtHZW06OlZlcnNpb24rLgogIGRlZiBzZWxmLmdlbV92ZXJz\\naW9uCiAgICBHZW06OlZlcnNpb24ubmV3IFZFUlNJT046OlNUUklORwogIGVu\\nZAoKICBtb2R1bGUgVkVSU0lPTgogICAgTUFKT1IgPSA3CiAgICBNSU5PUiA9\\nIDIKICAgIFRJTlkgID0gMAogICAgUFJFICAgPSAiYWxwaGEiCgogICAgU1RS\\nSU5HID0gW01BSk9SLCBNSU5PUiwgVElOWSwgUFJFXS5jb21wYWN0LmpvaW4o\\nIi4iKQogIGVuZAplbmQK\\n\"}"
+    end
+
+    allow(Net::HTTP).to receive(:get_response).and_return(mock_response)
+    github_path = "rails/rails/contents/version.rb"
+    response = Qippet::Extract.from_github(github_path)
+    expect(response).not_to be nil
+  end
 end
